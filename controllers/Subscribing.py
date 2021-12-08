807ed21a5c74bab2f0cdf02d546169b1ee51effe
gherman7755 <mr.mariarti@list.ru>
2021-12-08 17:39:01 +0300

    Update Subscribing.py

--- controllers/Subscribing.py
+++ controllers/Subscribing.py
@@ -1,68 +1,77 @@
-import random
+from abc import ABC, abstractmethod
+from random import randrange
+from typing import List
 
-from classes.Workers import WorkerFactory
-from classes.Menu import FactoryProducer as fp
-from classes.Tables import Tables
-from controllers.DataBase import DataBase, Check
-from controllers.Cheque import ParsedCheque, client_request
-from controllers.Delivery import Kitchen, Transport, OnlineOrder, client_order
-from controllers.Subscribing import RestaurantJournal, WantsToVisit, Other
+states = {1: "Closed", 2: "Opened", 3: "Event", 4: "Free Food", 5: "Repair"}
 
 
-def divide():
-    print(20 * '-')
+class IRestaurantJournal(ABC):
+
+    @abstractmethod
+    def subscribe(self, subscriber):
+        pass
+
+    @abstractmethod
+    def unsubscribe(self, subscriber):
+        pass
+
+    def notify(self):
+        pass
 
 
-w = WorkerFactory()
-worker = w.hire_worker("Gordon Ramsay", experience=50, type_of_worker="chief")
-new_worker = w.hire_worker("Jamie Oliver", 30, educated=False)
+class RestaurantJournal(IRestaurantJournal):
+    _state = None
+    _subscribers = []
 
-# Proxy. Checks if someone can access it
-db = DataBase([worker, new_worker])
-check = Check(db, "admin", "admin")
+    def subscribe(self, subscriber):
+        print("One new subscriber!")
+        self._subscribers.append(subscriber)
 
+    def unsubscribe(self, subscriber):
+        print("One unsubscriber!:(")
+        self._subscribers.remove(subscriber)
 
-factory = fp()
-italian_kitchen = factory.get_factory("italian")
-main_order = italian_kitchen.get_dish("main")
-main_order.cook()
+    def notify(self):
+        print("Notifying subs...")
+        for sub in self._subscribers:
+            pass
 
-moldavian_kitchen = factory.get_factory("moldavian")
-dessert_order = moldavian_kitchen.get_dish("dessert")
-dessert_order.cook()
-
-divide()
-
-first_table = Tables(1, 1500)
-second_table = first_table.clone()
-
-# Adapter
-cheque = ParsedCheque(1, first_table.number, 25, [main_order, dessert_order], first_table.cost, "Jane")
-print("First table ordered: ", client_request(cheque))
-
-main_order = moldavian_kitchen.get_dish("main")
-dessert_order = italian_kitchen.get_dish("dessert")
-
-# Facade
-transport = Transport()
-kitchen = Kitchen()
-order = OnlineOrder(kitchen, transport)
-client_order(order, [main_order, dessert_order], "Baker Street 221b")
-
-divide()
-
-# Observer
-journal = RestaurantJournal()
-number_subscribers = random.randint(1, 10)
-subs = []
-for i in range(number_subscribers):
-    sub = random.choice([WantsToVisit(), Other()])
-    subs.append(sub)
-    journal.subscribe(sub)
-
-journal.change_state()
-journal.change_state()
+    def change_state(self):
+        self._state = randrange(1, 6)
+        print(f"Restaurant now is in state {states[self._state]}")
+        self.notify()
 
 
-# print(first_table, second_table, sep='\n')
-# print("First Table and Second Table are one object: " + str(id(first_table) == id(second_table)))
+class Subscriber(ABC):
+    @abstractmethod
+    def update(self, journal):
+        pass
+
+
+class WantsToVisit(Subscriber):
+    def update(self, journal):
+        if journal._state >= 2 and journal._state != 5:
+            print("Updated for lovers!")
+
+
+class Other(Subscriber):
+    def update(self, journal):
+        if journal._state == 1 or journal._state == 5:
+            print("Updated for others!")
+
+
+if __name__ == "__main__":
+    journal = RestaurantJournal()
+    sub1 = WantsToVisit()
+    journal.subscribe(sub1)
+
+    sub2 = Other()
+    journal.subscribe(sub2)
+
+    journal.change_state()
+    journal.change_state()
+
+    journal.unsubscribe(sub2)
+
+    journal.change_state()
+
